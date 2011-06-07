52f6cc4c233f42d817e3745067e2704bdfe3c0cf
DavidKinder <david.kinder@david.kinder>
2011-06-07 21:46:28 +0100

    Fix IE version check

--- Inform7/ReportHtml.cpp
+++ Inform7/ReportHtml.cpp
@@ -324,7 +324,7 @@ void ReportHtml::SetIEPreferences(const char* path)
   {
     // IE9 has a nasty bug: IDocHostUIHandler::GetOptionKeyPath() is never called.
     // To work around this, we intercept and re-direct calls to open the IE registry key.
-    if (theApp.GetIEVersion() == 9.0)
+    if (theApp.GetIEVersion() >= 9.0)
     {
       HMODULE advadi = ::LoadLibrary("advapi32.dll");
       HMODULE mshtml = ::LoadLibrary("mshtml.dll");
