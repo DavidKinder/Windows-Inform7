9461740f39fcdeb119081b863dbe70551c149524
David Kinder <david.kinder@david.kinder>
2016-08-07 10:52:17 +0100

    Revised Windows build settings

--- Distribution_add/make_settings_windows.mk
+++ Distribution_add/make_settings_windows.mk
@@ -1,23 +1,23 @@
 # Make settings for integrating Inform's user interface and core software.
 # This is the Windows version, by David Kinder. To use, you will need
-# Cygwin with GCC 3.4 installed.
+# Cygwin with the old GCC 3.3 Cygwin/MingGW compiler installed.
 
 BUILTINCOMPS = ../Build/Compilers
 INTERNAL = ../Build/Internal
 BUILTINHTML = ../Build/Documentation
 BUILTINHTMLINNER = ../Build/Documentation/sections
 
-I6COMPILERNAME = inform6
+I6COMPILERNAME = inform6.exe
 
 INDOCOPTS = windows_app
 INRTPSOPTS = -nofont
 INTESTOPTS = -threads=2 -no-colours
 
-GCC = gcc
-GCCOPTS = -mno-cygwin -DPLATFORM_WINDOWS -O2 -I. -gdwarf-2
-GCCWARNINGS = -Wno-pointer-arith -Wno-unused-macros -Wno-shadow -Wno-cast-align -Wno-missing-noreturn -Wno-missing-prototypes -Wno-unused-parameter -Wno-padded
+GCC = gcc-3
+GCCOPTS = -DPLATFORM_WINDOWS -I. -mno-cygwin -gdwarf-2
+GCCWARNINGS = -Wno-pointer-arith -Wno-unused-macros -Wno-shadow -Wno-cast-align -Wno-missing-noreturn -Wno-missing-prototypes -Wno-unused-parameter -Wno-padded -Wno-unreachable-code-break -Wno-format-nonliteral -Wno-cast-qual
 CBLORBWARNINGS = -Wno-format-nonliteral
-LINK = gcc $(GCCOPTS) -ansi
+LINK = $(GCC) $(GCCOPTS) -g
 LINKEROPTS = -Wl,--large-address-aware
 ARTOOL = ar -r
 
