3d323a61afdc714a1e97635f630669058657a631
David Kinder <david.kinder@fisglobal.com>
2019-01-26 21:55:46 +0000

    Print stacks for x86 and x64

--- Inform7/StackTrace.cpp
+++ Inform7/StackTrace.cpp
@@ -58,8 +58,8 @@ bool GotFunctions(void)
   return true;
 }
 
-void PrintStackTrace(HANDLE process, HANDLE thread,
-  const CString& imageFile, LPVOID imageBase, DWORD imageSize, PCONTEXT context, std::ostream& log)
+void PrintStackTrace(HANDLE process, HANDLE thread, const CString& imageFile, LPVOID imageBase, DWORD imageSize,
+  DWORD machine, DWORD64 pc, DWORD64 framePtr, DWORD64 stackPtr, PVOID context, std::ostream& log)
 {
   // Load the debug help DLL, if available, and find entry points
   if (debugDll == 0)
@@ -78,17 +78,6 @@ void PrintStackTrace(HANDLE process, HANDLE thread,
   }
   if (!GotFunctions())
     return;
-
-  // Check that this is an appropriate  processor
-  SYSTEM_INFO si;
-  ::GetSystemInfo(&si);
-#ifdef _WIN64
-  if (si.wProcessorArchitecture != PROCESSOR_ARCHITECTURE_AMD64)
-    return;
-#else
-  if (si.wProcessorArchitecture != PROCESSOR_ARCHITECTURE_INTEL)
-    return;
-#endif
 
   // Set symbol options
   (*symSetOptions)(SYMOPT_DEFERRED_LOADS|SYMOPT_LOAD_LINES);
@@ -110,29 +99,17 @@ void PrintStackTrace(HANDLE process, HANDLE thread,
   }
 
   // Set up the initial stack frame
-  STACKFRAME64 frame;
-  ::ZeroMemory(&frame,sizeof frame);
+  STACKFRAME64 frame = { 0 };
   frame.AddrPC.Mode = AddrModeFlat;
   frame.AddrFrame.Mode = AddrModeFlat;
   frame.AddrStack.Mode = AddrModeFlat;
-#ifdef _WIN64
-  frame.AddrPC.Offset = context->Rip;
-  frame.AddrFrame.Offset = context->Rbp;
-  frame.AddrStack.Offset = context->Rsp;
-#else
-  frame.AddrPC.Offset = context->Eip;
-  frame.AddrFrame.Offset = context->Ebp;
-  frame.AddrStack.Offset = context->Esp;
-#endif
+  frame.AddrPC.Offset = pc;
+  frame.AddrFrame.Offset = framePtr;
+  frame.AddrStack.Offset = stackPtr;
 
   while (true)
   {
     // Get the next stack frame
-#ifdef _WIN64
-    DWORD machine = IMAGE_FILE_MACHINE_AMD64;
-#else
-    DWORD machine = IMAGE_FILE_MACHINE_I386;
-#endif
     if ((*stackWalk64)(machine,process,thread,&frame,context,
                        NULL,symFunctionTableAccess64,symGetModuleBase64,NULL) == 0)
       break;
@@ -185,13 +162,18 @@ void LogStackTrace(void)
   HANDLE process = ::GetCurrentProcess();
   HANDLE thread = ::GetCurrentThread();
 
-  // Get the thread's context
-  CONTEXT context;
-  ::ZeroMemory(&context,sizeof context);
+  // Create a stream to write to the log file
+  CString logPath = theOS.SHGetFolderPath(0,CSIDL_PERSONAL,NULL,SHGFP_TYPE_CURRENT);
+  logPath = logPath + LOG_FILE;
+  std::ofstream log(logPath);
+
+  // Get the thread's context and then print its stack trace
+  CONTEXT context = { 0 };
+  context.ContextFlags = CONTEXT_FULL;
 #ifdef _WIN64
   ::RtlCaptureContext(&context);
+  PrintStackTrace(process,thread,0,0,0,IMAGE_FILE_MACHINE_AMD64,context.Rip,context.Rbp,context.Rsp,&context,log);
 #else
-  context.ContextFlags = CONTEXT_FULL;
   {
     __asm    call x
     __asm x: pop eax
@@ -199,14 +181,8 @@ void LogStackTrace(void)
     __asm    mov context.Ebp, ebp
     __asm    mov context.Esp, esp
   }
+  PrintStackTrace(process,thread,0,0,0,IMAGE_FILE_MACHINE_I386,context.Eip,context.Ebp,context.Esp,&context,log);
 #endif
-
-  // Create a stream to write to the log file
-  CString logPath = theOS.SHGetFolderPath(0,CSIDL_PERSONAL,NULL,SHGFP_TYPE_CURRENT);
-  logPath = logPath + LOG_FILE;
-  std::ofstream log(logPath);
-
-  PrintStackTrace(process,thread,0,0,0,&context,log);
   log.close();
 }
 
@@ -214,21 +190,46 @@ void LogStackTrace(void)
 
 CString GetStackTrace(HANDLE process, HANDLE thread, DWORD exCode, const CString& imageFile, LPVOID imageBase, DWORD imageSize)
 {
-  CONTEXT context;
-  ::ZeroMemory(&context,sizeof context);
+  std::ostringstream log;
+  log << "Exception code 0x" << std::hex << exCode << std::endl;
+
+#ifdef _WIN64
+  // IS the process a 64-bit one, or a 32-bit one running under WOW (Windows On Windows)?
+  BOOL isWow64 = FALSE;
+  if (::IsWow64Process(process,&isWow64))
+  {
+    if (isWow64)
+    {
+      WOW64_CONTEXT context = { 0 };
+      context.ContextFlags = CONTEXT_FULL;
+      if (::Wow64GetThreadContext(thread,&context))
+      {
+        PrintStackTrace(process,thread,imageFile,imageBase,imageSize,
+          IMAGE_FILE_MACHINE_I386,context.Eip,context.Ebp,context.Esp,&context,log);
+      }
+    }
+    else
+    {
+      CONTEXT context = { 0 };
+      context.ContextFlags = CONTEXT_FULL;
+      if (::GetThreadContext(thread,&context))
+      {
+        PrintStackTrace(process,thread,imageFile,imageBase,imageSize,
+          IMAGE_FILE_MACHINE_AMD64,context.Rip,context.Rbp,context.Rsp,&context,log);
+      }
+    }
+  }
+#else
+  CONTEXT context = { 0 };
   context.ContextFlags = CONTEXT_FULL;
   if (::GetThreadContext(thread,&context))
   {
-    std::ostringstream log;
-#ifdef _WIN64
-    log << "Exception code 0x" << std::hex << exCode << " at PC=0x" << context.Rip << std::endl;
-#else
-    log << "Exception code 0x" << std::hex << exCode << " at PC=0x" << context.Eip << std::endl;
-#endif
-    PrintStackTrace(process,thread,imageFile,imageBase,imageSize,&context,log);
-    return CString(log.str().c_str());
+    PrintStackTrace(process,thread,imageFile,imageBase,imageSize,
+      IMAGE_FILE_MACHINE_I386,context.Eip,context.Ebp,context.Esp,&context,log);
   }
-  return CString("");
+#endif
+
+  return CString(log.str().c_str());
 }
 
 extern "C" void FatalError(void)
