7097a4caa8a8ca01b29686ac57e148790b0c21ce
David Kinder <david.kinder@fisglobal.com>
2019-01-26 22:06:11 +0000

    Fix x64 warnings in Scintilla

--- Inform7/Scintilla/win32/ScintillaWin.cxx
+++ Inform7/Scintilla/win32/ScintillaWin.cxx
@@ -569,7 +569,7 @@ static unsigned int SciMessageFromEM(unsigned int iMessage) {
 
 static UINT CodePageFromCharSet(DWORD characterSet, UINT documentCodePage) {
 	CHARSETINFO ci = { 0, 0, { { 0, 0, 0, 0 }, { 0, 0 } } };
-	BOOL bci = ::TranslateCharsetInfo((DWORD*)characterSet,
+	BOOL bci = ::TranslateCharsetInfo(reinterpret_cast<DWORD*>(static_cast<ptrdiff_t>(characterSet)),
 		&ci, TCI_SRCCHARSET);
 
 	UINT cp;
