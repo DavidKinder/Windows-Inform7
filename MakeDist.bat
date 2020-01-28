02c24bb4f381c5e755a0f23f72bf6442bc67a062
David Kinder <david.kinder@david.kinder>
2020-01-28 18:31:09 +0000

    Changes to source file archive

--- MakeDist.bat
+++ MakeDist.bat
@@ -1,15 +1,15 @@
 @echo off
 
 del Build\Inform7.pdb
+rmdir /s /q Build\GPUCache
 
 pushd \Programs
 "%ProgramFiles(x86)%\Zip\zip" -j \Temp\Windows_UI_source.zip Adv\Inform7\Inform7\COPYING
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\MakeDist.bat
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\BuildDate\*.vcproj Adv\Inform7\BuildDate\*.cpp
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Inform7\res\* Adv\Inform7\Inform7\*.cpp Adv\Inform7\Inform7\*.c Adv\Inform7\Inform7\*.h Adv\Inform7\Inform7\*.rc Adv\Inform7\Inform7\*.vcproj Adv\Inform7\Inform7\*.sln
+"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\BuildDate\*.vcxproj Adv\Inform7\BuildDate\*.cpp
+"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Inform7\res\* Adv\Inform7\Inform7\*.cpp Adv\Inform7\Inform7\*.h Adv\Inform7\Inform7\*.rc Adv\Inform7\Inform7\*.vcxproj Adv\Inform7\Inform7\*.filters Adv\Inform7\Inform7\*.sln
 "%ProgramFiles(x86)%\Zip\zip" -r \Temp\Windows_UI_source.zip Adv\Inform7\Inform7\Scintilla\*
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Installer\Inform7.* Adv\Inform7\Installer\*.bmp
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Installer\PlugIn\*.cpp Adv\Inform7\Installer\PlugIn\*.sln Adv\Inform7\Installer\PlugIn\*.vcproj
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Interpreters\Frotz\*.cpp Adv\Inform7\Interpreters\Frotz\*.vcproj Adv\Inform7\Interpreters\Frotz\*.sln
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Interpreters\Glulxe\*.cpp Adv\Inform7\Interpreters\Glulxe\*.h Adv\Inform7\Interpreters\Glulxe\*.vcproj Adv\Inform7\Interpreters\Glulxe\*.sln Adv\Inform7\Interpreters\Glulxe\Makefile
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Inform7\Interpreters\Git\*.cpp Adv\Inform7\Interpreters\Git\Makefile
@@ -21,13 +21,8 @@ pushd \Programs
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Glk\Git\*.c Adv\Glk\Git\*.h Adv\Glk\Git\README*
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Glk\Include\g*.h
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Adv\Glk\GlkDll\*.c Adv\Glk\GlkDll\Glk*.cpp Adv\Glk\GlkDll\Glk*.h
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\mfc\* Libraries\libpng\* Libraries\zlib\* Libraries\jpeg\* Libraries\hunspell\*
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\scalegfx\*.h Libraries\scalegfx\*.cpp Libraries\scalegfx\*.def Libraries\scalegfx\Makefile
+"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\mfc\* Libraries\scalegfx\*.h Libraries\scalegfx\*.cpp
 "%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\libmodplug\*
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\libvorbis\*
-"%ProgramFiles(x86)%\Zip\zip" -r \Temp\Windows_UI_source.zip Libraries\libvorbis\include\* Libraries\libvorbis\lib\*
-"%ProgramFiles(x86)%\Zip\zip" \Temp\Windows_UI_source.zip Libraries\libogg\*
-"%ProgramFiles(x86)%\Zip\zip" -r \Temp\Windows_UI_source.zip Libraries\libogg\include\* Libraries\libogg\src\*
 popd
 
 pushd Installer
