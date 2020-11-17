0bcc8b266db7a2799a7c525b3243333379bd96b3
David Kinder <david.kinder@fisglobal.com>
2020-11-17 18:07:02 +0000

    Regex help for find/replace dialogs

--- Inform7/FindReplaceDialog.cpp
+++ Inform7/FindReplaceDialog.cpp
@@ -2,6 +2,7 @@
 #include "FindReplaceDialog.h"
 #include "Inform.h"
 #include "Messages.h"
+#include "RichEdit.h"
 #include "UnicodeEdit.h"
 #include "resource.h"
 
@@ -13,12 +14,15 @@ IMPLEMENT_DYNAMIC(FindReplaceDialog, I7BaseDialog)
 
 BEGIN_MESSAGE_MAP(FindReplaceDialog, I7BaseDialog)
   ON_WM_CLOSE()
+  ON_WM_DESTROY()
+  ON_WM_DRAWITEM()
   ON_BN_CLICKED(IDC_FIND_NEXT, OnFindNext)
   ON_BN_CLICKED(IDC_FIND_PREVIOUS, OnFindPrevious)
   ON_EN_CHANGE(IDC_FIND, OnChangeFindText)
   ON_BN_CLICKED(IDC_REPLACE, OnReplace)
   ON_BN_CLICKED(IDC_REPLACE_ALL, OnReplaceAll)
   ON_EN_CHANGE(IDC_REPLACE_WITH, OnChangeReplaceWith)
+  ON_CBN_SELCHANGE(IDC_FIND_RULE, OnChangeFindRule)
 END_MESSAGE_MAP()
 
 FindReplaceDialog* FindReplaceDialog::Create(UINT id, CWnd* parentWnd)
@@ -31,6 +35,7 @@ FindReplaceDialog* FindReplaceDialog::Create(UINT id, CWnd* parentWnd)
     return NULL;
   }
   theApp.SetIcon(dialog);
+  dialog->PrepareHelp();
   return dialog;
 }
 
@@ -57,6 +62,38 @@ FindReplaceDialog::FindReplaceDialog(UINT id, CWnd* parentWnd) : I7BaseDialog(id
 {
   m_ignoreCase = TRUE;
   m_findRule = FindRule_Contains;
+  m_richText = NULL;
+  m_heightNormal = 0;
+  m_heightHelp = 0;
+}
+
+void FindReplaceDialog::PrepareHelp(void)
+{
+  CRect dlgRect;
+  GetWindowRect(dlgRect);
+  m_heightNormal = dlgRect.Height();
+  m_heightHelp = m_heightNormal;
+
+  // Figure out sizes for showing regular expression help
+  if (m_regexHelp.GetSafeHwnd())
+  {
+    if (m_richText == NULL)
+      m_richText = new RichDrawText();
+
+    CSize textSize = theApp.MeasureText("HELP",GetFont());
+    int helpLines = GetDlgItem(IDC_REPLACE) ? 11 : 10;
+
+    CRect helpRect;
+    m_regexHelp.GetWindowRect(helpRect);
+    int gap = dlgRect.bottom - helpRect.bottom;
+
+    ScreenToClient(helpRect);
+    helpRect.top = gap + helpRect.bottom;
+    helpRect.right = dlgRect.Width() - helpRect.left*2;
+    helpRect.bottom = helpRect.top + textSize.cy*helpLines;
+    m_regexHelp.MoveWindow(helpRect);
+    m_heightHelp += (gap + helpRect.Height());
+  }
 }
 
 void FindReplaceDialog::DoDataExchange(CDataExchange* pDX)
@@ -68,6 +105,8 @@ void FindReplaceDialog::DoDataExchange(CDataExchange* pDX)
     DDX_CBIndex(pDX,IDC_FIND_RULE,(int&)m_findRule);
   if (GetDlgItem(IDC_REPLACE_WITH))
     DDX_TextW(pDX, IDC_REPLACE_WITH, m_replaceWith);
+  if (GetDlgItem(IDC_REGEX_HELP))
+    DDX_Control(pDX, IDC_REGEX_HELP, m_regexHelp);
 }
 
 void FindReplaceDialog::OnCancel()
@@ -104,6 +143,89 @@ void FindReplaceDialog::OnClose()
   GetParentFrame()->SetActiveWindow();
 }
 
+void FindReplaceDialog::OnDestroy()
+{
+  delete m_richText;
+  m_richText = NULL;
+}
+
+void FindReplaceDialog::OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT di)
+{
+  if (nIDCtl == IDC_REGEX_HELP)
+  {
+    CRect helpRect(di->rcItem);
+    HDC hdc = 0;
+    HANDLE pb = ::BeginBufferedPaint(di->hDC,helpRect,BPBF_COMPATIBLEBITMAP,NULL,&hdc);
+    if (pb == 0)
+      return;
+    CDC* dc = CDC::FromHandle(hdc);
+    dc->FillSolidRect(helpRect,::GetSysColor(COLOR_BTNFACE));
+
+    const char* text1 =
+      "\\b Characters\\b0\\par\\par"
+      " .\\tab Any character\\par"
+      " \\\\b\\tab Word boundary\\par"
+      " \\\\d\\tab Any digit\\par"
+      " \\\\n\\tab Newline\\par"
+      " \\\\s\\tab Whitespace\\par"
+      " \\\\S\\tab Non-whitespace\\par"
+      " \\\\t\\tab Tab\\par"
+      " [A-Z]\\tab Character ranges\\par";
+    CString rtfText;
+    rtfText.Format("{\\rtf1\\ansi{\\fs%d%s}}",
+      theApp.GetFontSize(InformApp::FontSystem)*2,text1);
+    m_richText->SetTextRTF(rtfText);
+    m_richText->DrawText(*dc,helpRect);
+
+    const char* text2 =
+      "\\b Patterns\\b0\\par\\par"
+      " x?\\tab 0 or 1 of 'x'\\par"
+      " x*\\tab 0 or more of 'x'\\par"
+      " x+\\tab 1 or more of 'x'\\par"
+      " x\\{2\\}\\tab Exactly 2 of 'x'\\par"
+      " x\\{2,\\}\\tab 2 or more of 'x'\\par"
+      " x\\{,2\\}\\tab 0-2 of 'x'\\par"
+      " x\\{2,4\\}\\tab 2-4 of 'x'\\par";
+    rtfText.Format("{\\rtf1\\ansi{\\fs%d%s}}",
+      theApp.GetFontSize(InformApp::FontSystem)*2,text2);
+    m_richText->SetTextRTF(rtfText);
+    CRect textRect;
+    textRect.SetRectEmpty();
+    textRect.right = helpRect.Width();
+    m_richText->SizeText(*dc,textRect);
+    textRect.MoveToX((helpRect.right - textRect.Width())/2);
+    m_richText->DrawText(*dc,textRect);
+
+    const char* text3 = GetDlgItem(IDC_REPLACE) ?
+      "\\b Expressions\\b0\\par\\par"
+      " (x)\\tab Group expression\\par"
+      " x|y\\tab x or y\\par"
+      " ^\\tab Start of the line\\par"
+      " $\\tab End of the line\\par\\par"
+      "\\b Replace With\\b0\\par\\par"
+      " \\\\1\\tab First group\\par"
+      " \\\\2\\tab Second group\\par"
+      :
+      "\\b Expressions\\b0\\par\\par"
+      " (x)\\tab Group expression\\par"
+      " x|y\\tab x or y\\par"
+      " ^\\tab Start of the line\\par"
+      " $\\tab End of the line\\par";
+    rtfText.Format("{\\rtf1\\ansi{\\fs%d%s}}",
+      theApp.GetFontSize(InformApp::FontSystem)*2,text3);
+    m_richText->SetTextRTF(rtfText);
+    textRect.SetRectEmpty();
+    textRect.right = helpRect.Width();
+    m_richText->SizeText(*dc,textRect);
+    textRect.MoveToX(helpRect.right - textRect.Width());
+    m_richText->DrawText(*dc,textRect);
+
+    ::EndBufferedPaint(pb,TRUE);
+  }
+  else
+    CWnd::OnDrawItem(nIDCtl,di);
+}
+
 void FindReplaceDialog::OnFindNext()
 {
   UpdateData(TRUE);
@@ -138,6 +260,31 @@ void FindReplaceDialog::OnChangeReplaceWith()
 {
   UpdateData(TRUE);
   EnableActions();
+}
+
+void FindReplaceDialog::OnChangeFindRule()
+{
+  UpdateData(TRUE);
+  if (m_findRule == FindRule_Regex)
+  {
+    CRect dlgRect;
+    GetWindowRect(dlgRect);
+    dlgRect.bottom = dlgRect.top + m_heightHelp;
+    MoveWindow(dlgRect);
+
+    m_regexHelp.ModifyStyle(0,WS_VISIBLE);
+    Invalidate();
+  }
+  else if (m_regexHelp.IsWindowVisible())
+  {
+    CRect dlgRect;
+    GetWindowRect(dlgRect);
+    dlgRect.bottom = dlgRect.top + m_heightNormal;
+    MoveWindow(dlgRect);
+
+    m_regexHelp.ModifyStyle(WS_VISIBLE,0);
+    Invalidate();
+  }
 }
 
 void FindReplaceDialog::EnableActions(void)
--- Inform7/FindReplaceDialog.h
+++ Inform7/FindReplaceDialog.h
@@ -3,6 +3,8 @@
 #include "BaseDialog.h"
 #include "FindInFiles.h"
 
+class RichDrawText;
+
 class FindReplaceDialog : public I7BaseDialog
 {
   DECLARE_DYNAMIC(FindReplaceDialog)
@@ -18,6 +20,7 @@ public:
 
 protected:
   FindReplaceDialog(UINT id, CWnd* parentWnd);
+  void PrepareHelp(void);
 
   virtual void DoDataExchange(CDataExchange* pDX);
   virtual void OnCancel();
@@ -25,12 +28,15 @@ protected:
   DECLARE_MESSAGE_MAP()
 
   afx_msg void OnClose();
+  afx_msg void OnDestroy();
+  afx_msg void OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT di);
   afx_msg void OnFindNext();
   afx_msg void OnFindPrevious();
   afx_msg void OnChangeFindText();
   afx_msg void OnReplace();
   afx_msg void OnReplaceAll();
   afx_msg void OnChangeReplaceWith();
+  afx_msg void OnChangeFindRule();
 
 private:
   void EnableActions(void);
@@ -38,4 +44,8 @@ private:
   CStringW m_findText, m_replaceWith;
   BOOL m_ignoreCase;
   FindRule m_findRule;
+
+  CStatic m_regexHelp;
+  RichDrawText* m_richText;
+  int m_heightNormal, m_heightHelp;
 };
