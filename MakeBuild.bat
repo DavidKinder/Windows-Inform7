8dd49d4e2444839276629d861fce288b57c84ffc
David Kinder <david.kinder@david.kinder>
2022-04-12 15:15:25 +0100

    Clear out internal area of build before copying

--- MakeBuild.bat
+++ MakeBuild.bat
@@ -1,4 +1,5 @@
 @echo off
 copy Distribution\inform\inform6\Tangled\inform6.exe Build\Compilers
 copy Distribution\inform\inform7\Tangled\inform7.exe Build\Compilers
+rmdir /s /q Build\Internal
 xcopy /s /q /i /y Distribution\inform\inform7\Internal Build\Internal
