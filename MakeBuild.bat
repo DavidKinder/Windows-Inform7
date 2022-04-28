c11819a2de1cb37e70e187e8ed920216d1bfad08
David Kinder <david.kinder@david.kinder>
2022-04-28 06:43:27 +0100

    Remove MakeBuild.bat, use make forceintegration in Distribution/inform instead

--- MakeBuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-copy Distribution\inform\inblorb\Tangled\inblorb.exe Build\Compilers
-copy Distribution\inform\inform6\Tangled\inform6.exe Build\Compilers
-copy Distribution\inform\inform7\Tangled\inform7.exe Build\Compilers
-copy Distribution\intest\Tangled\intest.exe Build\Compilers
-rmdir /s /q Build\Internal
-xcopy /s /q /i /y Distribution\inform\inform7\Internal Build\Internal
