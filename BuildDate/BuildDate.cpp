011a9eb3dc25dd9afd09b29a8f4129e8feade7e5
David Kinder <david.kinder@david.kinder>
2022-05-13 19:58:10 +0100

    Only regenerate Build.h if it will have actually changed

--- BuildDate/BuildDate.cpp
+++ BuildDate/BuildDate.cpp
@@ -1,7 +1,11 @@
-#include <cstdio>
-#include <ctime>
+#include <stdio.h>
+#include <time.h>
+#include <string>
 
-int main(int, char**)
+char informVersion[32] = "?";
+std::string buildHeader;
+
+void createOutput(void)
 {
   time_t now;
   time(&now);
@@ -32,10 +36,10 @@ int main(int, char**)
   char monthYear[256];
   strftime(monthYear,256,"%B %Y",timeNow);
 
-  char informVersion[32], line[256];
+  char line[256];
   FILE* contentsFile = fopen("../Distribution/inform/inform7/Contents.w","rt");
   if (contentsFile == NULL)
-    return 0;
+    exit(1);
   while (feof(contentsFile) == 0)
   {
     fgets(line,256,contentsFile);
@@ -43,13 +47,44 @@ int main(int, char**)
   }
   fclose(contentsFile);
 
-  FILE* outFile = fopen("Build.h","wt");
-  fprintf(outFile,"#define BUILD_DATE \"%d%s %s\"\n",timeNow->tm_mday,afterNum,monthYear);
-  fprintf(outFile,"#define INFORM_VER \"%s\"\n",informVersion);
-  fclose(outFile);
+  sprintf(line,"#define BUILD_DATE \"%d%s %s\"\n",timeNow->tm_mday,afterNum,monthYear);
+  buildHeader = line;
+  sprintf(line,"#define INFORM_VER \"%s\"\n",informVersion);
+  buildHeader += line;
+}
 
-  outFile = fopen("..\\Installer\\Inform7.nsh","wt");
-  fprintf(outFile,"!define INFORM_VER %s\n",informVersion);
-  fclose(outFile);
+std::string getCurrent(void)
+{
+  std::string currentHeader;
+
+  FILE* inFile = fopen("Build.h","rt");
+  if (inFile != NULL)
+  {
+    char line[256];
+
+    fgets(line,256,inFile);
+    currentHeader = line;
+    fgets(line,256,inFile);
+    currentHeader += line;
+    fclose(inFile);
+  }
+  return currentHeader;
+}
+
+int main(int, char**)
+{
+  createOutput();
+  std::string currentHeader = getCurrent();
+  if (currentHeader != buildHeader)
+  {
+    printf("Creating new Build.h...\n");
+    FILE* outFile = fopen("Build.h","wt");
+    fputs(buildHeader.c_str(),outFile);
+    fclose(outFile);
+
+    outFile = fopen("..\\Installer\\Inform7.nsh","wt");
+    fprintf(outFile,"!define INFORM_VER %s\n",informVersion);
+    fclose(outFile);
+  }
   return 0;
 }
